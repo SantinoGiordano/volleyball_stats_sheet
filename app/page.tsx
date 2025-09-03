f258c94ed30f3998b6fe0b996bae26561ceabe0d
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-03 12:12:13 -0400

    temp code block for mapping

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       Volleyball stats
       <div className="overflow-x-auto">
+        {playerData.map((player: any) => (
+          <div>{prop.name}</div>
+        ))}
         <table className="table table-zebra">
           {/* head */}
           <thead>
