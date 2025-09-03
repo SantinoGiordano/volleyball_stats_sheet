718ec2fc77655adc73d094150a983fcd658278b9
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-03 12:12:01 -0400

    added a types page for object data

---
+++ types/types.tsx
@@ -0,0 +1,15 @@
+export interface PlayerStats {
+    _id: string;
+    order: number;
+    name: string;
+    number: number;
+    pos: string;
+    kills: number;
+    hittingPercent: number;
+    digs: number;
+    sets: number;
+    soloblocks: number;
+    aces: number;
+    hittingErr: number;
+    err: number;
+}
