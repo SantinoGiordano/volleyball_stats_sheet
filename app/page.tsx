e9c69a3016fb9de6caed0fd8e301051f2598dfe9
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-03 12:19:22 -0400

    added a key for map

--- app/page.tsx
+++ app/page.tsx
@@ -16,49 +16,48 @@ export default function Home() {
       Volleyball stats
       <div className="overflow-x-auto">
         {playerData.map((player: PlayerStats) => (
-        
+          <table key={player._id} className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>#</th>
+                <th>POS</th>
+                <th>Kills</th>
+                <th>HT %</th>
+                <th>Digs</th>
+                <th>ACE</th>
+                <th>Sets</th>
+                <th>Solo Blocks</th>
+                <th>ERR</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>12</td>
+                <td>Blue</td>
+              </tr>
+              {/* row 2 */}
+              <tr>
+                <th>2</th>
+                <td>Hart Hagerty</td>
+                <td>24</td>
+                <td>Purple</td>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>8</td>
+                <td>Red</td>
+              </tr>
+            </tbody>
+          </table>
         ))}
-        <table className="table table-zebra">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>#</th>
-              <th>POS</th>
-              <th>Kills</th>
-              <th>HT %</th>
-              <th>Digs</th>
-              <th>ACE</th>
-              <th>Sets</th>
-              <th>Solo Blocks</th>
-              <th>ERR</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>12</td>
-              <td>Blue</td>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>24</td>
-              <td>Purple</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>8</td>
-              <td>Red</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
   );
